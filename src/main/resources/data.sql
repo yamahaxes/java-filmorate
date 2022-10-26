7e51945c0fe50d55ddeb37364ca6fc772ebbda75
yamahaxes <bulbenkoevgen@yandex.ru>
2022-10-26 20:32:07 +0700

    feat: Добавить schema.sql и data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO MPA (MPA_NAME) values ('G');
+INSERT INTO MPA (MPA_NAME) values ('PG');
+INSERT INTO MPA (MPA_NAME) values ('PG-13');
+INSERT INTO MPA (MPA_NAME) values ('R');
+INSERT INTO MPA (MPA_NAME) values ('NC-17');
+
+INSERT INTO GENRES(GENRE_NAME) values ( 'Комедия' );
+INSERT INTO GENRES(GENRE_NAME) values ( 'Драма' );
